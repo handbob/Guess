ec0dcd9ba0c76fef29016de9c31c022150e79540
handbob <handbob@icloud.com>
2022-07-17 19:46:50 +0200

    make guess game

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Guess;
+
+using System;
+
+public class Program {
+    public static void Main(String[] args) {
+        Console.Write("Think a number from range 1 to 10.\nEnter number: ");
+        var number = Int32.Parse(Console.ReadLine());
+        Console.WriteLine();
+        var thinkingNumber = Random.Shared.Next(1, 10);
+
+        if (number == thinkingNumber) {
+            Console.WriteLine("You have found match.");
+        }
+        else {
+            do {
+                Console.Write("Try again.\nEnter number: ");
+                number = Int32.Parse(Console.ReadLine());
+                Console.WriteLine();
+            }
+            while (number != thinkingNumber);
+        }
+        Console.WriteLine();
+        Console.WriteLine($"Searching number {thinkingNumber}");
+        Console.WriteLine($"Your number {number}");
+    }
+};
